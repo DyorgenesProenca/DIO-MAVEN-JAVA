1854b907f8d2a2c4fb899060362ac8cfdd21ae2a
Dyorgenes <dyorgenes2018@gmail.com>
2026-06-24 17:57:23 -0300

    exemplos de Assertions

---
+++ Hellou_Word_Maven/src/test/java/AssertionsTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class AssertionsTest {
+    @Test
+    void validarLancamentos(){
+
+        int[] primeiroLancamento = {10, 20, 30, 40, 50};
+        int[] segundoLancamento = {10, 20, 30, 40, 50};
+
+        Assertions.assertArrayEquals(primeiroLancamento,segundoLancamento);
+    }
+    @Test
+    void validarSeObjetoEstaNulo(){
+        Pessoa pessoa = null;
+        Assertions.assertNull(pessoa);
+
+        pessoa = new Pessoa("Luciano", LocalDate.now());
+
+        Assertions.assertNotNull(pessoa);
+    }
+
+    @Test
+    void validarNumerosDeTiposDiferentes(){
+        double valor = 5.0;
+        double outroValor = 5.0;
+
+        Assertions.assertEquals(valor,outroValor);
+    }
+}
