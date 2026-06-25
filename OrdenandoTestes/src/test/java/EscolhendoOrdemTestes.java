8ed1894a7d13a5be56281125ce0cee5189f6049c
Dyorgenes <dyorgenes2018@gmail.com>
2026-06-25 16:04:47 -0300

    Ordem de Testes de forma Aleatoria

--- OrdenandoTestes/src/test/java/EscolhendoOrdemTestes.java
+++ OrdenandoTestes/src/test/java/EscolhendoOrdemTestes.java
@@ -1,24 +1,21 @@
 import org.junit.jupiter.api.*;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestMethodOrder(MethodOrderer.Random.class)
 public class EscolhendoOrdemTestes {
 
-    @Order(1)
     @Test
     void validaFluxoD(){
         Assertions.assertTrue(true);
     }
-    @Order(3)
     @Test
     void validaFluxoC(){
         Assertions.assertTrue(true);
     }
-    @Order(4)
+
     @Test
     void validaFluxoB(){
         Assertions.assertTrue(true);
     }
-    @Order(2)
     @Test
     void validaFluxoA(){
         Assertions.assertTrue(false);
